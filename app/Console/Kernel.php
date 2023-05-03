aad3e2fbf2df4c71f00bed8cb341345a606e5d49
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-05-03 18:30:11 -0400

    add security tool

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('security-portal-client:sync')->environments('production')->hourly();
     }
 
     /**
