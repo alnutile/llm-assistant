ba9f3f141dcb0b0f2d50c6b73abe2d9b0a056762
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-05-03 14:24:33 -0400

    run ci-cd

---
+++ app/helpers.php
@@ -0,0 +1,8 @@
+<?php
+
+if (! function_exists('test_helper')) {
+    function test_helper()
+    {
+        return 'foo';
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,15 +5,25 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
-
+import Toast, { TYPE }  from "vue-toastification";
+import "vue-toastification/dist/index.css";
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
-
+const toastOptions = {
+    toastDefaults: {
+        // ToastOptions object for each type of toast
+        [TYPE.DEFAULT]: {
+            timeout: 3000,
+            hideProgressBar: true,
+        }
+    }
+};
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(Toast, toastOptions)
             .use(ZiggyVue, Ziggy)
             .mount(el);
     },
