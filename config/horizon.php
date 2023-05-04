2273a88a0b40e9f6a131a14727cf448ce5615ba3
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-05-04 07:48:39 -0400

    patch mail queue

--- config/horizon.php
+++ config/horizon.php
@@ -182,7 +182,7 @@ return [
     'defaults' => [
         'supervisor-1' => [
             'connection' => 'redis',
-            'queue' => ['default'],
+            'queue' => ['default', 'mail'],
             'balance' => 'auto',
             'autoScalingStrategy' => 'time',
             'maxProcesses' => 1,
