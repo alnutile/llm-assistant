366aa83456584d7e157476e6edff1526d8e6eb6b
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-05-04 05:42:09 -0400

    add magic login

--- config/magic-login.php
+++ config/magic-login.php
@@ -5,5 +5,5 @@ use App\Models\User;
 return [
 
     'user_table' => User::class,
-    'redirect' => env("MAGIC_LOGIN_REDIRECT", 'dashboard'),
+    'redirect' => env('MAGIC_LOGIN_REDIRECT', 'dashboard'),
 ];
