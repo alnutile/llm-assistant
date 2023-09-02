cd67c65a0c6fdd6238d4aa2a0a549a5b2413e5aa
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-09-02 12:28:24 -0400

    add llm function

---
+++ database/migrations/2023_09_02_162641_add_llm_function_.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\LlmFunction;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $params = get_fixture_v2('content_to_voice.json', false);
+        LlmFunction::firstOrCreate([
+            'label' => 'content_to_voice',
+        ], [
+            'label' => 'content_to_voice',
+            'description' => 'Use the string of content to create a voice audio version of the content using an external service',
+            'parameters' => $params,
+            'active' => 1,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
