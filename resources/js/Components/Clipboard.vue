36b2572d44e3050913c0ad2979e7772ceec3248c
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-09-04 07:25:46 -0400

    add the clipboard

---
+++ resources/js/Components/Clipboard.vue
@@ -0,0 +1,29 @@
+<template>
+    <button @click="copyToClipboard(content)"
+            class="py-1 px-2 bg-red-600 rounded-lg hover:bg-red-500 text-sm
+            font-semibold
+            text-white">
+        Copy
+    </button>
+</template>
+
+<script setup>
+
+import {useToast} from "vue-toastification";
+const toast = useToast();
+
+const props = defineProps({
+    content: String
+})
+
+const copyToClipboard = (content) => {
+    navigator.clipboard
+        .writeText(content)
+        .then(() => {
+            toast("Copied")
+        })
+        .catch((err) => {
+            console.error("Failed to copy to clipboard:", err);
+        });
+};
+</script>
--- resources/js/Pages/Messages/Components/ChildCard.vue
+++ resources/js/Pages/Messages/Components/ChildCard.vue
@@ -3,6 +3,8 @@
         <div class="mb-4 prose prose-lg" v-html="message.content_formatted">
         </div>
 
+
+
         <div class="flex justify-end gap-2 items-center">
             <div>{{ message.created_at}}</div>
             <div v-if="message.role === 'user'">
@@ -15,6 +17,7 @@
                     <img class="h-8 w-8 rounded-full object-cover" src="/images/robot.png" alt="" />
                 </span>
             </div>
+            <Clipboard v-if="message.role !== 'user'" :content="message.content"></Clipboard>
         </div>
     </div>
 </template>
@@ -23,6 +26,7 @@
 import SecondaryButton from "@/Components/SecondaryButtonLink.vue";
 import {computed} from "vue";
 import { usePage } from "@inertiajs/vue3";
+import Clipboard from "@/Components/Clipboard.vue";
 
 const props = defineProps({
     message: Object,
