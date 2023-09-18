f3f726426e9e2257efcd0251e750b27ca41bc8f2
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-09-18 09:24:16 -0400

    text for image

---
+++ database/seeders/TextToImageSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\LlmFunction;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TextToImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $parameters = get_fixture_v2('text_for_image.json', false);
+        LlmFunction::create([
+            'label' => 'text_for_image',
+            'description' => 'Use this function to convert and image to text',
+            'parameters' => $parameters,
+        ]);
+    }
+}
